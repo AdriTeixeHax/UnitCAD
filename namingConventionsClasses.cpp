31117715a41aa858a97e54e33e033f45cfa8f3aa
AdriTeixeHax <adrianteixeiradeuna@gmail.com>
2023-10-19 12:29:37 +0200

    Updated naming conventions

--- namingConventionsClasses.cpp
+++ namingConventionsClasses.cpp
@@ -1,4 +1,49 @@
-#include <cstdint>
+#ifndef NAMING_CONVENTIONS // Uso de flags en vez de #pragma once, a pesar de que la mayoría de los compiladores ya lo soporten, para evitar errores.
+#define NAMING_CONVENTIONS
+
+// *.h file code here
+
+#endif
+
+/* ORDEN DE INCLUSIÓN DE LIBRERÍAS: */
+// Librerías de la STL
+// Librerías de otras dependencias (Gráfica, GUI, matemáticas...)
+// Librerías propias
+
+// STL
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+/* #include <cstdint> */ // No incluir librerías que ya formen parte de otras (cstdint ya está incluida en iostream)
+
+// OpenGL graphics
+#ifndef GLEW_STATIC
+#define GLEW_STATIC
+#endif // Preferentemente incluir las guards en la propia configuración del proyecto
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// OpenGL math
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// GUI
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
+/* EXPRESIONES CONSTANTES */
+#define glAssert(x) if (!(x)) __debugbreak(); // Uso de define para funciones, en camelCase como si estuviese definida en otro lugar.
+#define EXAMPLE_PATH "paths/example.png"      // Uso de define para strings ya que es más rápido que un constexpr
+constexpr uint16_t EXAMPLE_VAR = 269;         // Uso de constexpr para variables de un tamaño o tipo concreto.
+#define EXAMPLE_VAR_2 269U;                   // Alternativa: utilizar las terminaciones f, U, etc. para indicar el tipo cuando se quiera usar un define.
+// Las expresiones numéricas en mayúsculas con guiones bajos de separación.
+
+namespace Namespace // Namespaces en PascalCase como las clases.
+{
+    uint8_t exampleFunc(void) { return 0; }
+};
 
 class Example
 {
